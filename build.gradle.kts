13578371feaade3caaa473b0bcac126a25bac998
Daniel Espendiller <daniel@espendiller.net>
2020-01-17 17:36:02 +0100

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.4.15"
+    java
+}
+
+group = "de.espend.idea.localization"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2019.2.4"
+}
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
